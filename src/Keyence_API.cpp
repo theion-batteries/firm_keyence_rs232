4d97d9ccef89f417c2913179f380be16f282eeaa
sami-theion <sami@theion.de>
2022-04-11 17:02:53 +0200

    finished keyence apis

--- src/Keyence_API.cpp
+++ src/Keyence_API.cpp
@@ -18,14 +18,17 @@ int Keyence_base::HeadsArray[12];
 
 // list heads availble
 void Keyence_base::listHeads()
-{
-    Serial.println("listing heads in vcontainer");
+{   
+    
+    transformArrayToVector(HeadsArray,NumUsedHeads);
+    NumUsedHeads.resize(HeadsCount);
+    Serial.println("listing heads in vector container");
       // For loop using iterators
     if(!NumUsedHeads.empty())
     {
       for (std::vector<int>::iterator it = NumUsedHeads.begin(); it != NumUsedHeads.end(); it++)
       {
-    Serial.println("next head number:");
+      Serial.println("next head number:");
       Serial.println(*it);
       }
     }
@@ -73,24 +76,42 @@ serialHandler->begin(baudrate);
 //get a output value of single head: head number format is 01,02,03... but param is given as int 1,2,3...
 double Keyence_rs232_interface::getValueSingleOutputHead(int output_head_Nr)
 {
-  if(serialHandler->available()>0)
-  {
+    String Zero="0";
+    String Soutput_head_Nr=String(output_head_Nr);
     //write the get value command
     String command="mesure_value_outputN";
     String cmd=findCommand(command,commands);
-    if (output_head_Nr<9){String(output_head_Nr)="0"+String(output_head_Nr);}
-    cmd+=String(output_head_Nr);
+    if (output_head_Nr<9){
+      Soutput_head_Nr=Zero+Soutput_head_Nr;
+      }
+    cmd+=Soutput_head_Nr;
     serialHandler->print(cmd+CR);
+    Serial.println("sending command:");
+    Serial.println(cmd+CR);
+  if(serialHandler->available()>0)
+  {
     // Read data from rs232 port
-    String Response=serialHandler->readString();
+    String Response=serialHandler->readStringUntil('\r');
     #ifdef DEBUG
     // write the data to the other port
-    Serial.println("response:");
+    Serial.println("full response:");
     Serial.println(Response);
-    Serial.println("response to double");
+    if(Response.indexOf("-")) //if there is a negtaiv sign
+    {
+      double negativ = -1.0;
     //MS,OUT01,value CR: remove M->,: 9
     Response.remove(0,9); //remove default response
-    Response.toDouble(); // type cast to doule
+    Response.c_str(); // type cast to double 
+    double Res = double(Response.toDouble());
+    Res*=negativ;//time * negtaiv
+    Serial.println("response to negativ double");
+    Serial.println(Res);
+    return Res;
+    } 
+    //MS,OUT01,value CR: remove M->,: 9
+    Response.remove(0,9); //remove default response
+    Response.toDouble(); // type cast to double * negtaiv    
+    Serial.println("response to double");
     Serial.println(Response);
     #endif
     return Response.toDouble();
@@ -99,114 +120,110 @@ double Keyence_rs232_interface::getValueSingleOutputHead(int output_head_Nr)
 //get output multiple heads in this format: "0-12" example: head 1,2 and 3 will be 111000000000
 double* Keyence_rs232_interface::getValueMultipleOutputHead(String HeadsArray)
 {
-  if(serialHandler->available()>0)
-  {
+  int NumOfOutputs = 0;
     //write the get value command
+    if(NumOfOutputs<1)
+    {
+      for (auto& element:HeadsArray)
+      {
+        if(element=='1')
+        {
+          NumOfOutputs++;
+        }
+      }
+    }
+    double* Values = new double[NumOfOutputs];
+    //double Values[NumOfOutputs];
+    String valuesHolder="";
+    int ValuesCounter=0;
+    Serial.println("number of heads");
+    Serial.println(NumOfOutputs);
     String command="mesure_value_multipleN";
     String cmd=findCommand(command,commands);
     //cmd:MM,010010000000
     cmd+=String(HeadsArray);
-    serialHandler->print(cmd);
+    Serial.println("command sent:");
+    Serial.println(cmd);
+    serialHandler->println(cmd);
+  if(serialHandler->available()>0)
+  {
     // Read data from rs232 port
-    String Response=serialHandler->readString();
+    String Response=serialHandler->readStringUntil('\r');
     #ifdef DEBUG
     // write the data to the other port
     Serial.println("response:");
     Serial.println(Response);
     #endif
     // Response: MM,010010000000,value[,value,value]: 
-    Response.remove(0,21); //remove default response
-    // iterate string of values: take , as delimiter for value
-    // extract value and add them to array
-    double Values[12];
-    char* valuesHolder="";
-    int ValuesCounter=0;
-    for (int i=0; i<Response.length(); i++)
-    {
-  // we check if the this char is digit, +, -, . or ,
-  // example: [,+0.0123,-1.258,+8.235]
-    if (isdigit(Response[i])) // if is char digit
-      {
-        valuesHolder+=Response[i]; //add char to valuesHolder
+    Response.replace(cmd,""); //remove default response
+    Serial.println(Response);
+    // iterate response and extract values
+
+    for (int i=0; i<Response.length();i++)
+    {// ,val1,val2,val3,val4,val5
+      if(Response[i]==',')
+      {        
+        valuesHolder=Response.substring(i+1,Response.indexOf(',',i+1));
+        Serial.println("value holder got");
+        Serial.println(valuesHolder);
+        Values[ValuesCounter]=valuesHolder.toDouble();
+        ValuesCounter++;
+        valuesHolder="";
       }
-    if (Response[i]=='+')
-      {
-        //ignore + sign
-      }
-    if (Response[i]=='-')
-      {
-        valuesHolder+=Response[i];
-      }
-    if (Response[i]=='.')
-      {
-        valuesHolder+=Response[i];
-      }
-    if (Response[i]==',')
-      {
-        if(i>2)
-        {
-          ++ValuesCounter; 
-            Values[ValuesCounter]=String(valuesHolder).toDouble();
-        }
-      }
+      if(ValuesCounter==NumOfOutputs) break;
     }
-    Serial.println(*Values);
+    for (int i =0; i<NumOfOutputs; i++)
+    {
+      Serial.println("extracted values");
+      Serial.println(*(Values + i));
+    }
     return Values;
   }
 }
 // get output all
 double* Keyence_rs232_interface::getValueOutputHeadAll()
 {
-  if(serialHandler->available()>0)
-  {
+    double* Values = new double[HeadsCount];
+    //double Values[NumOfOutputs];
+    String valuesHolder="";
+    int ValuesCounter=0;
+    Serial.println("number of heads");
+    Serial.println(HeadsCount);
     //write the get value command
     String command="mesure_value_All";
     String cmd=findCommand(command,commands);
     serialHandler->print(cmd+CR);
+    if(serialHandler->available()>0)
+  {
     // Read data from rs232 port
-    String Response=serialHandler->readString();
+    String Response=serialHandler->readStringUntil('\r');
     #ifdef DEBUG
     // write the data to the other port
     Serial.println("response:");
     Serial.println(Response);
     #endif
-    // MA,value[,value,value]: remove M->e:8
-    Response.remove(0,8); //remove default response
-    // iterate string of values: take , as delimiter for value
-    // extract value and add them to array
-    double Values[12];
-    char* valuesHolder="";
-    int ValuesCounter=0;
-    for (int i=0; i<Response.length(); i++)
-    {
-  // we check if the this char is digit, +, -, . or ,
-  // example: [,+0.0123,-1.258,+8.235]
-    if (isdigit(Response[i])) // if is char digit
-      {
-        valuesHolder+=Response[i]; //add char to valuesHolder
+    // MA,value[,value,value]: 
+    Response.replace(cmd,""); //remove default response
+    Serial.println(Response);
+    // iterate response and extract values
+    for (int i=0; i<Response.length();i++)
+    {// ,val1,val2,val3,val4,val5
+      if(Response[i]==',')
+      {        
+        valuesHolder=Response.substring(i+1,Response.indexOf(',',i+1));
+        Serial.println("value holder got");
+        Serial.println(valuesHolder);
+        Values[ValuesCounter]=valuesHolder.toDouble();
+        ValuesCounter++;
+        valuesHolder="";
       }
-    if (Response[i]=='+')
-      {
-        //ignore + sign
-      }
-    if (Response[i]=='-')
-      {
-        valuesHolder+=Response[i];
-      }
-    if (Response[i]=='.')
-      {
-        valuesHolder+=Response[i];
-      }
-    if (Response[i]==',')
-      {
-        if(i>2)
-        {
-          ++ValuesCounter; 
-            Values[ValuesCounter]=String(valuesHolder).toDouble();
-        }
-      }
+      if(ValuesCounter==HeadsCount) break;
     }
-    Serial.println(*Values);
+    for (int i =0; i<HeadsCount; i++)
+    {
+      Serial.println("extracted values");
+      Serial.println(*(Values + i));
+    }
     return Values;
   }
 }
@@ -214,37 +231,48 @@ double* Keyence_rs232_interface::getValueOutputHeadAll()
 // set general mode
 void Keyence_rs232_interface::setGeneralMode()
 {
-  if(serialHandler->available()>0)
-  {
+
     //write the get value command
     String command="set_general_mode";
     String cmd=findCommand(command,commands);
     serialHandler->print(cmd+CR);
+  delay(100); //this delay is obligatory to clear buffer
+  if(serialHandler->available()>0)
+  {
     // Read data from rs232 port
-    String Response=serialHandler->readString();
+    String Response=serialHandler->readStringUntil('\r');
     #ifdef DEBUG
     // write the data to the other port
     Serial.println("response:");
     Serial.println(Response);
+    
     #endif
   }
+  serialHandler->write("");
+  serialHandler->flush();
+
 }
 // set communication mode
 void Keyence_rs232_interface::setCommunicationMode()
 {
-  if(serialHandler->available()>0)
-  {
+
     //write the get value command
     String command="set_communication_mode";
     String cmd=findCommand(command,commands);
     serialHandler->print(cmd+CR);
+  delay(100); //this delay is obligatory to clear buffer
+
+  if(serialHandler->available()>0)
+  {
     // Read data from rs232 port
-    String Response=serialHandler->readString();
+    String Response=serialHandler->readStringUntil('\r');
     #ifdef DEBUG
     // write the data to the other port
     Serial.println("response:");
     Serial.println(Response);
     #endif
   }
+  serialHandler->write("");
+  serialHandler->flush();
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -19,16 +19,17 @@
 // composition is better than inheritance 
 void setup() {
   Serial.begin(BAUDRATE);
-  Serial.println(Keyence_base::NumUsedHeads.size());
-  Serial.println(Keyence_base::HeadsCount);
-  Keyence_base::printArrayElements(Keyence_base::HeadsArray);
-  Keyence_base::transformArrayToVector(Keyence_base::HeadsArray,Keyence_base::NumUsedHeads);
   Keyence_base::listHeads();
+  DS_x.setKeyenceCom();
 }
 
 void loop() { 
-  DS_x.getCurrentDistance();
-  DS_y.getCurrentDistance();
-  DS_z.getCurrentDistance();
-  delay(500); // delay for next mesurement for test
+  //DS_x.getCurrentDistance();
+  //DS_y.getCurrentDistance();
+  //DS_z.getCurrentDistance();
+  DS_x.IkeyenceRS232->getValueMultipleOutputHead("111000000000");
+  //DS_x.IkeyenceRS232->getValueOutputHeadAll();
+  delay(300);
+
+
 }
